d7ffe16f37fe5bcccdb3ec82a9b26a72e7969825
Collier <v_collier@ats-hr420-dm23.fc.ca>
2019-10-21 17:42:34 -0400

    adding files

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    host: "localhost",
-    port: 3306, // 8889 for mac
-    uname: "root",
-    pword: "root", // leave blank for windows
-    database: "db_card"
-}
-
-module.exports = config;
--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-// this is a partially revealing module pattern - just a variation on what we've already done
-
-const myVM = (() => {
-    // get the user buttons and fire off an async DB query with Fetch
-
-})();
