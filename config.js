fa7fb9ddf64763a2b5489d8cbcd4315e32e52baf
Victoria Collier <victoriacollier@Victorias-MacBook-Pro.local>
2019-10-30 16:38:18 -0400

    fixing

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    host: "localhost",
-    port: 3000, // 8889 for mac
-    uname: "root",
-    pword: "root", // leave blank for windows
-    database: "db_card"
-}
-
-module.exports = config;
