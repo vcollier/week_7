ea83226e8816c255b6e95633f757fbcb2a5a93c6
Victoria Collier <victoriacollier@Victorias-MacBook-Pro.local>
2019-10-30 16:41:01 -0400

    adding recent files

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "localhost",
+    port: 3000, // 8889 for mac
+    uname: "root",
+    pword: "root", // leave blank for windows
+    database: "db_card"
+}
+
+module.exports = config;
